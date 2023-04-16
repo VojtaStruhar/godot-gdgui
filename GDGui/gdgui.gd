ad2471d1f88999afd3fc486c66e758952efa1180
Vojtěch Struhár <vojtechstruhar@gmail.com>
2023-04-16 23:31:32 +0200

    Added static text labels

--- GDGui/gdgui.gd
+++ GDGui/gdgui.gd
@@ -5,7 +5,7 @@ class_name GDGui
 
 var _layout_stack = []
 
-var _elements_cache: Dictionary = {}
+var _elements_registry: Dictionary = {}
 
 var __button_presses: Dictionary = {}
 
@@ -17,19 +17,19 @@ func _process(delta: float) -> void:
 	for key in __button_presses:
 		__button_presses[key] = false
 
-func _add_element(id, element: Control):
+func _add_element(id: Variant, element: Control):
 	var last = main
 	if _layout_stack.size() > 0:
 		last = _layout_stack[_layout_stack.size() - 1]
 	
-	_elements_cache[id] = element
+	_elements_registry[id] = element
 	last.add_child(element)
 	
 
 func button(title: String = "Button", options: Dictionary = {}) -> bool:
 	var button_id = options["id"] if "id" in options else "button_" + title
 	
-	if (button_id not in _elements_cache):
+	if (button_id not in _elements_registry):
 		var btn = Button.new()
 		btn.pressed.connect(func(): __button_presses[button_id] = true)
 		btn.text = title
@@ -37,3 +37,13 @@ func button(title: String = "Button", options: Dictionary = {}) -> bool:
 		_add_element(button_id, btn)
 	
 	return __button_presses[button_id] if button_id in __button_presses else false
+
+## Static label identified by its text. Do not use for labels with dynamic text.
+func label(text: String) -> void:
+	var label_id = "label_static_" + str(text.hash())
+	
+	if label_id not in _elements_registry:
+		var l = Label.new()
+		l.name = "Static label"
+		l.text = text
+		_add_element(label_id, l)
--- test_world.gd
+++ test_world.gd
@@ -3,8 +3,11 @@ extends Node3D
 @onready var gdgui: GDGui = $CanvasLayer/Control
 
 func _process(delta: float) -> void:
-	if (gdgui.button("Vojta", {"id": 1} )):
+	gdgui.label("This is a GDGui test!")
+	gdgui.label("Yo this is sick")
+	
+	if (gdgui.button("Vojta", { "id": 1 })):
 		print("Print 1 - you want to see me")
 		
 	if (gdgui.button("Vojta")):
-		print("Print 2 - oops")
+		print("Print 2")
