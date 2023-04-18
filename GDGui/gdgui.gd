6b5b26814b143bcb97cd1ef95522acce0b728432
Vojtěch Struhár <vojtechstruhar@gmail.com>
2023-04-18 17:14:34 +0200

    Added simple button and label test

--- GDGui/gdgui.gd
+++ GDGui/gdgui.gd
@@ -19,6 +19,7 @@ func _process(_delta: float) -> void:
 		_cleanup()
 	
 	_call_count_stack = [0]
+	_layout_stack = []
 
 
 func _ready() -> void:
@@ -30,16 +31,15 @@ func label(text: String) -> void:
 	if current is Label:
 		current.text = text
 	else:
-		if current is Node:
-			print(_call_count_stack, " <-> Replacing ", current.name," (", current.get_class(), ") with a label")
-			current.queue_free()
+		if current != null:
+			print(_call_count_stack, "-> Replacing (", typeof(current), ") with a label")
+			_remove_current_element()
 		else:
 			print(_call_count_stack, " + Creating a label")
 
 		var l = Label.new()
 		l.text = text
 		_add_element(l)
-		_dom[_get_call_count()] = l
 	
 	_increase_call_count()
 
@@ -51,17 +51,17 @@ func button(text: String) -> bool:
 	if current is Button:
 		current.text = text
 	else:
-		if current is Node:
-			print(_call_count_stack, " <-> Replacing ", current.name, " (", current.get_class(), ") with a button")
-			current.queue_free()
+		if current != null:
+			print(_call_count_stack, "-> Replacing (", typeof(current), ") with a button")
+			_remove_current_element()
 		else:
-			print(_call_count_stack, " + Creating a button")
+			print(_call_count_stack, " + Creating a button ")
 		
+
 		var b = Button.new()
 		b.text = text
 		b.pressed.connect(func(): __button_presses[button_id] = true)
 		_add_element(b)
-		_dom[_get_call_count()] = b
 	
 	
 	_increase_call_count()
@@ -82,12 +82,20 @@ func begin_horizontal() -> void:
 		var h = HBoxContainer.new()
 		_add_element(h)
 		_layout_stack.push_back(h)
-		_dom[_get_call_count()] = h
 	
 	_increase_call_count()
+	
+	var hopefully_dict = _get_current_element()
+	if not hopefully_dict is Dictionary:
+		_add_element({})
+	
+	_call_count_stack.push_back(0)
 
 func end_horizontal() -> void:
 	var h = _layout_stack.pop_back()
+	_call_count_stack.pop_back()
+	_increase_call_count()
+	
 	if not h is HBoxContainer:
 		printerr("[GDGui] Called end_horizontal, but the topmost layout wasn't a HBoxContainer: ", h)
 
@@ -106,7 +114,6 @@ func begin_vertical() -> void:
 		var v = VBoxContainer.new()
 		_add_element(v)
 		_layout_stack.push_back(v)
-		_dom[_get_call_count()] = v
 	
 	_increase_call_count()
 
@@ -131,7 +138,6 @@ func begin_panel() -> void:
 		var p = PanelContainer.new()
 		_add_element(p)
 		_layout_stack.push_back(p)
-		_dom[_get_call_count()] = p
 	
 	_increase_call_count()
 
@@ -142,20 +148,35 @@ func end_panel() -> void:
 		printerr("[GDGui] Called end_panel, but the topmost layout wasn't a PanelContainer: ", p)
 
 
-func _add_element(e: Control) -> void:
-	_get_parent_layout().add_child(e)
+func _add_element(e: Variant) -> void:
+	if e is Control:
+		_get_parent_layout().add_child(e)
+		
+	var destination = _dom
+	for cc in _call_count_stack.slice(0, _call_count_stack.size() - 1):
+		destination = destination[cc]
+	
+	destination[_call_count_stack[-1]] = e
 
 
 # Reads the element for this call_count from the dom
 func _get_current_element():
-	var element = _dom
-	for cc in _call_count_stack:
-		if cc not in element:
+	var subtree = _dom
+	for cc in _call_count_stack.slice(0, _call_count_stack.size() - 1):
+		if cc not in subtree:
 			return null
-		element = _dom[cc]
-	# Dont return freed object, etc
-	if not is_instance_valid(element):
+		if not subtree[cc] is Dictionary:
+			return null
+		subtree = subtree[cc]
+	
+	var cc = _call_count_stack[-1]
+	if cc not in subtree:
 		return null
+	
+	var element = subtree[cc]
+	if element is Control and not is_instance_valid(element):
+		return null
+	
 	return element
 
 
@@ -178,17 +199,38 @@ func _cleanup() -> void:
 		var current = _get_current_element()
 		
 		if current != null:
-			print(_call_count_stack, " Deleting ", current)
-			if current is Node:
-				current.queue_free()
-			
-			var el = _dom
-			for cc in _call_count_stack.slice(0, _call_count_stack.size() - 1):
-				if cc in el:
-					el = el[cc]
-				else: print(cc, " as in ", _call_count_stack, " not found in ", el, ", (dom): ", _dom)
-			el[_call_count_stack[-1]] = null
-			
+			print(_call_count_stack, " Cleanup: Deleting ", current)
+			_remove_current_element()
 			_increase_call_count()
 		else:
 			_call_count_stack.pop_back()
+
+
+func _remove_current_element() -> void:
+	var subtree = _dom
+	for cc in _call_count_stack.slice(0, _call_count_stack.size() - 1):
+		if cc not in subtree:
+			printerr("removing ", _call_count_stack, " failed. ", _dom)
+			return
+		if not subtree[cc] is Dictionary:
+			printerr("removing ", _call_count_stack, " failed. ", _dom)
+			return
+		
+		subtree = subtree[cc]
+	
+	var cc = _call_count_stack[-1]
+	if cc not in subtree:
+		printerr("removing ", _call_count_stack, " failed. ", _dom)
+		return
+	
+	var element = subtree[cc]
+	if element is Control:
+		element.queue_free()
+	else:
+		# Dictionary - destroy all its children too
+		for key in element:
+			_call_count_stack.push_back(key)
+			_remove_current_element()
+			_call_count_stack.pop_back()
+	
+	subtree.erase(cc)
--- test_world.gd
+++ test_world.gd
@@ -4,29 +4,21 @@ extends Node3D
 
 var lesgo = false
 
-func _process(_delta: float) -> void:
+
+func gui():
 	gdgui.label("lesgo: " + str(lesgo))
 	
 	if gdgui.button("Toggle"):
 		lesgo = not lesgo
-	
-	
+		print("[toggle to ", lesgo, "]", gdgui._dom, "\n")
+
 	if lesgo:
 		gdgui.label("Nice")
-	
+
 	gdgui.begin_horizontal()
-	
-	gdgui.button("Useless")
-	
-	gdgui.begin_panel()
-	gdgui.begin_vertical()
-	gdgui.button("Useless 2")
-	gdgui.label("Label in vertical")
-	gdgui.end_vertical()
-	gdgui.end_panel()
-	
+	gdgui.label("Absolute bottom!")
 	gdgui.end_horizontal()
-	
-	
-	gdgui.label("Survive!")
-	
+
+func _process(_delta: float) -> void:
+	gui()
+	pass
